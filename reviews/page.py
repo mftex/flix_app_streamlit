0d042c8cc13b9acdae365f4535d6e44b4d3602d1
Marcelo Teixeira <marceloftex@gmail.com>
2026-01-10 08:22:28 -0300

    Implementando interação com a API no endpoint de Reviews

--- reviews/page.py
+++ reviews/page.py
@@ -1,29 +1,51 @@
 import pandas as pd
 import streamlit as st
 from st_aggrid import AgGrid
-
-
-reviews = [
-    {
-        'id': 1,
-        'stars': 5,
-    },
-    {
-        'id': 2,
-        'stars': 4,
-    },
-    {
-        'id': 3,
-        'stars': 3,
-    }
-
-]
-
+from reviews.service import ReviewService
+from movies.service import MovieService
 
 def show_reviews():
-    st.write("Lista de Avaliações:")
-    
-    AgGrid(pd.DataFrame(reviews),
-           reload_data=True,
-           key='reviews_grid',
-           show_toolbar=True,)
+    review_service = ReviewService()
+    reviews = review_service.get_reviews()
+
+    if reviews:
+        st.write("Lista de Avaliações:")
+
+        reviews_df = pd.json_normalize(reviews)
+        
+        AgGrid(reviews_df,
+            reload_data=True,
+            key='reviews_grid',
+            show_toolbar=True,)
+    else:
+        st.warning('Nenhuma avaliação encontrada.')
+
+    st.divider()
+
+    st.title('Cadastrar Nova Avaliação')
+
+    movie_service = MovieService()
+    movies = movie_service.get_movies()
+    movie_titles = {movie['title']: movie['id'] for movie in movies}
+
+    selected_movie_title = st.selectbox('Filme', list(movie_titles.keys()))
+
+    stars = st.number_input(
+        label='Estrelas',
+        min_value=0,
+        max_value=5,
+        step=1
+    )
+
+    comment = st.text_area('Comentário')
+
+    if st.button('Cadastrar'):
+        new_review = review_service.create_reviews(
+            movie=movie_titles[selected_movie_title],
+            stars=stars,
+            comment=comment
+        )
+        if new_review:
+            st.rerun()
+        else:
+            st.error('Erro ao cadastrar a avaliação.')
